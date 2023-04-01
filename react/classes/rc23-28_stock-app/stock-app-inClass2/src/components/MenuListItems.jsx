dc567db1c69a43fb73e8f0eec7c89172c5aa074c
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-04-01 16:22:36 +0200

    icons

--- react/classes/rc23-28_stock-app/stock-app-inClass2/src/components/MenuListItems.jsx
+++ react/classes/rc23-28_stock-app/stock-app-inClass2/src/components/MenuListItems.jsx
@@ -5,6 +5,23 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import MailIcon from '@mui/icons-material/Mail';
+import MenuIcon from '@mui/icons-material/Menu';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import StoreIcon from '@mui/icons-material/Store';
+import StarsIcon from '@mui/icons-material/Stars';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+
+const icons = [
+
+    {
+        title: "Dashboard",
+        icon: <MailIcon/>,
+        url: "/stock/"
+    }
+]
 
 const MenuListItems = () => {
   return (
@@ -13,7 +30,7 @@ const MenuListItems = () => {
       <ListItem key={text} disablePadding>
         <ListItemButton>
           <ListItemIcon>
-            {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            
           </ListItemIcon>
           <ListItemText primary={text} />
         </ListItemButton>
