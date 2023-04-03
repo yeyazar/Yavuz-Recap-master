67409453fa483091fa9bf4e20cadee8a6e867946
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-04-03 23:24:58 +0200

    useStockCall

---
+++ react/classes/rc23-28_stock-app/stock-app-inClass2/src/hooks/useStockCall.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+
+const useStockCall = () => {
+  const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+  
+  const getStockData = async (url) => {
+    const BASE_URL = "http://12233.fullstack.clarusway.com/";
+    dispatch(fetchStart());
+
+    try {
+      const { data } = await axios(`${BASE_URL}stock/${url}/`, {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      });
+      dispatch(getSuccess({ data, url }));
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+
+  return { getStockData };
+};
+
+export default useStockCall;
