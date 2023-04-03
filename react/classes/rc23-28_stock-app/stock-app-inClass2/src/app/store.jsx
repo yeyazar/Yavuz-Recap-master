81f9bf603465222e5fe6c85f2016b1a5f9fc7388
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-04-03 23:16:43 +0200

    firms

--- react/classes/rc23-28_stock-app/stock-app-inClass2/src/app/store.jsx
+++ react/classes/rc23-28_stock-app/stock-app-inClass2/src/app/store.jsx
@@ -1,8 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import stockReducer from "../features/stockSlice";
-import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 const persistConfig = {
   key: "root",
@@ -16,6 +26,12 @@ const store = configureStore({
     auth: persistedReducer,
     stock: stockReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
 });
 
--- react/classes/rc23-28_stock-app/stock-app-inClass2/src/pages/Firms.jsx
+++ react/classes/rc23-28_stock-app/stock-app-inClass2/src/pages/Firms.jsx
@@ -1,9 +1,44 @@
-
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 
 const Firms = () => {
-  return (
-    <div>Firms</div>
-  )
-}
+  const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-export default Firms
+  const getFirms = async () => {
+    const BASE_URL = "http://12233.fullstack.clarusway.com/";
+    dispatch(fetchStart());
+    const url = "firms"
+    try {
+      const { data } = await axios(`${BASE_URL}stock/firms/`, {
+        headers: {
+          Authorization: `Token ${token}`,
+        },
+      });
+      dispatch(getSuccess({data, url}));
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail());
+    }
+  };
+
+  useEffect(() => {
+    getFirms();
+  }, []);
+
+  return (
+    <div>
+      <Typography variant="h3" color="error" mb={3}>
+        Firm
+      </Typography>
+
+      <Button variant="contained">New Firm</Button>
+    </div>
+  );
+};
+
+export default Firms;
