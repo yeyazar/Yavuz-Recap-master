d94a2ea76948df3d51476690d58fdf2ee14316bd
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-04-04 00:07:46 +0200

    feat: added axiosPublic

--- react/classes/rc23-28_stock-app/stock-app-inClass2/src/hooks/useAxios.jsx
+++ react/classes/rc23-28_stock-app/stock-app-inClass2/src/hooks/useAxios.jsx
@@ -4,6 +4,9 @@ import axios from "axios";
 const useAxios = () => {
   const { token } = useSelector((state) => state.auth);
 
+  const axiosPublic = axios.create({
+    baseURL: "http://12233.fullstack.clarusway.com/",
+  });
   const axiosWithToken = axios.create({
     baseURL: "http://12233.fullstack.clarusway.com/",
     headers: { Authorization: `Token ${token}` },
