b5c142212f57c9a97a9be55fb15e1325e04d1d06
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-04-03 23:32:13 +0200

    feat: shortened Firms

--- react/classes/rc23-28_stock-app/stock-app-inClass2/src/pages/Firms.jsx
+++ react/classes/rc23-28_stock-app/stock-app-inClass2/src/pages/Firms.jsx
@@ -2,31 +2,8 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { useEffect } from "react";
 import useStockCall from "../hooks/useStockCall";
-// import axios from "axios";
-// import { useDispatch, useSelector } from "react-redux";
-// import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 
 const Firms = () => {
-  // const { token } = useSelector((state) => state.auth);
-  // const dispatch = useDispatch();
-
-  // const getFirms = async () => {
-  //   const BASE_URL = "http://12233.fullstack.clarusway.com/";
-  //   dispatch(fetchStart());
-  //   const url = "firms"
-  //   try {
-  //     const { data } = await axios(`${BASE_URL}stock/firms/`, {
-  //       headers: {
-  //         Authorization: `Token ${token}`,
-  //       },
-  //     });
-  //     dispatch(getSuccess({data, url}));
-  //   } catch (error) {
-  //     console.log(error);
-  //     dispatch(fetchFail());
-  //   }
-  // };
-
   const {getStockData} = useStockCall()
 
   useEffect(() => {
