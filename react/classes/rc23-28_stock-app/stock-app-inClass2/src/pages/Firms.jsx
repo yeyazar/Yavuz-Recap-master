a56de6de25556ebf27c514f6b9b0dac096ff1377
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-04-03 23:31:24 +0200

    feat: added useStockCall to firms

--- react/classes/rc23-28_stock-app/stock-app-inClass2/src/pages/Firms.jsx
+++ react/classes/rc23-28_stock-app/stock-app-inClass2/src/pages/Firms.jsx
@@ -1,33 +1,36 @@
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import axios from "axios";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+import useStockCall from "../hooks/useStockCall";
+// import axios from "axios";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 
 const Firms = () => {
-  const { token } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
+  // const { token } = useSelector((state) => state.auth);
+  // const dispatch = useDispatch();
 
-  const getFirms = async () => {
-    const BASE_URL = "http://12233.fullstack.clarusway.com/";
-    dispatch(fetchStart());
-    const url = "firms"
-    try {
-      const { data } = await axios(`${BASE_URL}stock/firms/`, {
-        headers: {
-          Authorization: `Token ${token}`,
-        },
-      });
-      dispatch(getSuccess({data, url}));
-    } catch (error) {
-      console.log(error);
-      dispatch(fetchFail());
-    }
-  };
+  // const getFirms = async () => {
+  //   const BASE_URL = "http://12233.fullstack.clarusway.com/";
+  //   dispatch(fetchStart());
+  //   const url = "firms"
+  //   try {
+  //     const { data } = await axios(`${BASE_URL}stock/firms/`, {
+  //       headers: {
+  //         Authorization: `Token ${token}`,
+  //       },
+  //     });
+  //     dispatch(getSuccess({data, url}));
+  //   } catch (error) {
+  //     console.log(error);
+  //     dispatch(fetchFail());
+  //   }
+  // };
+
+  const {getStockData} = useStockCall()
 
   useEffect(() => {
-    getFirms();
+    getStockData("firms");
   }, []);
 
   return (
