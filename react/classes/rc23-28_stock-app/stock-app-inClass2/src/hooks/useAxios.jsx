44061e8e6d6d665132d550140ebdb57d651629a3
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-04-04 00:08:49 +0200

    feat: added axiosPublic to return

--- react/classes/rc23-28_stock-app/stock-app-inClass2/src/hooks/useAxios.jsx
+++ react/classes/rc23-28_stock-app/stock-app-inClass2/src/hooks/useAxios.jsx
@@ -7,12 +7,13 @@ const useAxios = () => {
   const axiosPublic = axios.create({
     baseURL: "http://12233.fullstack.clarusway.com/",
   });
+  
   const axiosWithToken = axios.create({
     baseURL: "http://12233.fullstack.clarusway.com/",
     headers: { Authorization: `Token ${token}` },
   });
 
-  return { axiosWithToken };
+  return { axiosWithToken, axiosPublic };
 };
 
 export default useAxios;
