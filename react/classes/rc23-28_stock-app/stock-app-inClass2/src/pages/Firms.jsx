1a4073e1539bdb0c39dcf81c14ce4679878cbaba
yeyazar <52497034+yeyazar@users.noreply.github.com>
2023-04-04 04:23:29 +0200

    feat: added FirmModal

--- react/classes/rc23-28_stock-app/stock-app-inClass2/src/pages/Firms.jsx
+++ react/classes/rc23-28_stock-app/stock-app-inClass2/src/pages/Firms.jsx
@@ -1,16 +1,21 @@
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useStockCall from "../hooks/useStockCall";
 import { useSelector } from "react-redux";
 import { Grid } from "@mui/material";
 import FirmCard from "../components/FirmCard";
 import { flex } from "../styles/globalStyles";
+import FirmModal from "../components/modals/FirmModal";
 
 const Firms = () => {
   const { getStockData } = useStockCall();
 
   const { firms } = useSelector((state) => state.stock);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     getStockData("firms");
@@ -24,9 +29,11 @@ const Firms = () => {
         Firm
       </Typography>
 
-      <Button variant="contained" sx={{ mb: "1rem" }}>
+      <Button variant="contained" sx={{ mb: "1rem" }} onClick={handleOpen}>
         New Firm
       </Button>
+
+      <FirmModal open={open} handleClose={handleClose}/>
 
       <Grid container sx={flex}>
         {firms?.map((firm) => (
